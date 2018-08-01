9c8122b2f41267655b6246f32f3b2699f9f30ae8
Onur Akkuş <31278081+Wiceriniel@users.noreply.github.com>
2018-08-01 20:39:24 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h> 
+#include <windows.h>
+
+void check();
+void yukari();
+void asagi();
+int sonTahmin;
+int rastgele; 
+int denemeSayisi = 0;
+
+int main() {
+
+    setlocale(LC_ALL, "Turkish"); 
+    SetConsoleTitle( TEXT("SAYI TAHMiN OYUNU") );
+    system("@cls||clear");
+    srand(time(NULL)); 
+    rastgele=rand()%1000;
+
+    printf("\nSayý gir [0-1000] : ");
+
+
+    scanf("%d", &sonTahmin);
+    check();
+
+    return 0;
+}
+
+void check() {
+
+    if (sonTahmin == rastgele) {
+        system("@cls||clear");
+        denemeSayisi++;
+        printf("\nTebrikler! %d sayýsýný %d. denemede buldun.\n\nÇýkmak için herhangi bir tuþa bas.\n", rastgele, denemeSayisi);
+        getch();
+    } else 
+    
+    if (sonTahmin > rastgele) {
+        system("@cls||clear");
+        denemeSayisi++;
+        asagi();
+        printf("\n\nSayý gir [0-1000] : ");
+        scanf("%d", &sonTahmin);
+        check();
+    } else
+
+    if (sonTahmin < rastgele) {
+        system("@cls||clear");
+        denemeSayisi++;
+        yukari();
+        printf("\n\nSayý gir [0-1000] : ");
+        scanf("%d", &sonTahmin);
+        check();
+    }
+    
+}
+
+void yukari() {
+
+    printf("\n           /\\");
+    printf("\n          /  \\");
+    printf("\n         /    \\          Son tahminin: %d", sonTahmin);
+    printf("\n        /      \\");
+    printf("\n       /        \\");
+    printf("\n      '---.  .---'");
+    printf("\n          |  |");
+    printf("\n          |  |");
+    printf("\n          |  |");
+    printf("\n          |  |");
+    printf("\n          '--'");
+
+}
+
+void asagi() {
+
+    printf("\n          .--.");
+    printf("\n          |  |");
+    printf("\n          |  |           Son tahminin: %d", sonTahmin);
+    printf("\n          |  |");
+    printf("\n          |  |");
+    printf("\n      .---'  '---.");
+    printf("\n       \\        /");
+    printf("\n        \\      /");
+    printf("\n         \\    /");
+    printf("\n          \\  /");
+    printf("\n           \\/");
+}
